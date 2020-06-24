96c60c2328df20ebe6d477d6de42c25887379710
Roman Volodchenkov <32496866+RomanVolodchenkov@users.noreply.github.com>
2020-06-24 22:57:08 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Начало функции, которая проверяет браузеры на поддержку webp формата и есл все OK, то использует формат webP
+function testWebP(callback) {
+
+	var webP = new Image();
+	webP.onload = webP.onerror = function () {
+		callback(webP.height == 2);
+	};
+	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+
+testWebP(function (support) {
+
+	if (support == true) {
+		document.querySelector('body').classList.add('webp');
+	} else {
+		document.querySelector('body').classList.add('no-webp');
+	}
+});
+// Конец функции, которая проверяет браузеры на поддержку webp формата и есл все OK, то использует формат webP
